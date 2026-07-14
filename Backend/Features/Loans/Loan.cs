a581f3be9441fa5be677d3cbee34a9715946044a
LoximtoMedina <crjosuem911@gmail.com>
2026-07-13 19:32:58 -0600

    feat: add backend project structure again with models, controllers, and configuration files

---
+++ Backend/Features/Loans/Loan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Loan{
+
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int BookId { get; set; }
+    public DateTime LoanDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public int Status { get; set; }
+    public int Quantity { get; set; }
+    public string? Penalization { get; set;
+
+}
