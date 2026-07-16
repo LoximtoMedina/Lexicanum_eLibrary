35cdc948ff5f527c861ac4a81603f84f046316e8
LoximtoMedina <crjosuem911@gmail.com>
2026-07-15 22:02:53 -0600

    Actualiza origen permitido de CORS a localhost:5173
    Se ha modificado la política "AllowReactApp" en Program.cs para aceptar solicitudes desde "http://localhost:5173" en lugar de "http://localhost:3000". Esto adapta la API al nuevo origen del frontend.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddOpenApi();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
-        policy => policy.WithOrigins("http://localhost:3000")
+        policy => policy.WithOrigins("http://localhost:5173")
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
