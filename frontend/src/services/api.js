508570f8b3ecc851167fd627bec21e913d8bbd4c
YafetFz <ayafetfz@gmail.com>
2026-07-21 13:30:18 -0600

    Actualiza la URL base del API para apuntar al servidor correcto

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // Asegúrate de que este puerto sea el mismo que usa tu backend cuando corres en local
-    baseURL: '100.69.251.29:7017' 
+    baseURL: '100.107.57.23:7017' 
 });
 
 // Ejemplo de exportación de una función para obtener libros
